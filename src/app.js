4de93f8675198606f4e85bde66facf365101ba22
Rohit Nagote <rohitnagote01@gmail.com>
2026-08-23 04:29:29 +0530

    created user schema

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const connectDB = require("./config/database");
 const app = express();
+const user = require("./models/user");
+
+app.post('/signup', async (req, res) => {
+    const newuser = new user({
+       firstname: "aren",
+       lastname: "yegar",
+       email: "aren.yegar@example.com",
+       password: "password123",
+       age: 25,
+       gender: "male"
+     });
+
+    await newuser.save();
+    res.send("User created successfully");
+
+});
 
 connectDB().then(() => {
     console.log("Database connected successfully");
--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     await mongoose.connect(
-        "mongodb+srv://rohitnagote:rohit%40510@rocluster.rlxa9bs.mongodb.net/"
+        "mongodb+srv://rohitnagote:rohit%40510@rocluster.rlxa9bs.mongodb.net/devsph"
     );
 };
 
