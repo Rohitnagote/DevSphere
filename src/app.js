47031310fdd57b048eaa1f9971541c45d503cc23
Rohit Nagote <rohitnagote01@gmail.com>
2026-08-26 05:57:56 +0530

    created api

--- src/app.js
+++ src/app.js
@@ -16,6 +16,48 @@ app.post('/signup', async (req, res) => {
 
 });
 
+
+// to get user by email
+app.get('/user', async (req, res) => {
+    const useremail = req.body.email;
+    try{
+        const user = await User.find({email: useremail});
+        if(user.length==0){
+            res.send("User not found"+err.message);
+        }else{
+            res.send(user);
+        }
+    }catch(err){
+        res.send("error occured"+ err.message);
+    }
+
+});
+
+// to get all users
+app.get('/feed', async(req, res) => {
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }catch(err){
+        res.send("error occured"+ err.message);
+    }
+});
+
+// to delete user by id
+app.delete('/user', async(req, res) => {
+    const userid = req.body.userid;
+    try{
+        const user = await User.findByIdAndDelete(userid);
+        if(!user){
+            res.send("User not found");
+        }else{
+            res.send("User deleted successfully");
+        }
+    }catch(err){
+        res.send("error occured"+ err.message);
+    }
+});
+
 connectDB().then(() => {
     console.log("Database connected successfully");
     app.listen(3000, () => {
