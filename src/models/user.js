5a7b4f669fffc3e226f1b9b8bba301364048c8fc
Rohit Nagote <rohitnagote01@gmail.com>
2026-08-27 23:49:49 +0530

    validations added

--- src/models/user.js
+++ src/models/user.js
@@ -3,22 +3,45 @@ const mongoose = require('mongoose');
 const userschema = new mongoose.Schema({
     firstname: {
         type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 30,
     },
     lastname: {
         type: String,
     },
     email: {
         type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
     },
     password: {
         type: String,
+        required: true,
     },
     age: {
         type: Number,
+        min: 18,
     },
     gender:{
         type: String,
+        validate(value) {
+            if (!["male", "female", "others"].includes(value)) {
+                throw new Error ("Gender must be male female or others");
+            }
+
+        }    
+
     },
+    about: {
+        type: String,
+        default: "No information provided",
+    },
+    skills: {
+        type: [String],
+    }
 });
 
 const User = mongoose.model("User", userschema);
