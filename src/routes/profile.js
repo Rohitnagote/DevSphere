7666d812fb4f6631bbba862f8398b6ccf6d90e4a
Rohit Nagote <rohitnagote01@gmail.com>
2026-09-03 23:00:58 +0530

    password api

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -3,6 +3,7 @@ const { validateEditProfileData } = require('../utils/validation.js');
 
 const profileRouter = express.Router();
 const { userAuth } = require('../middleware/auth.js');
+const User = require('../models/user.js');
 
 
 profileRouter.get('/profile/view', userAuth, async (req, res) => {
@@ -34,5 +35,21 @@ profileRouter.patch('/profile/edit', userAuth, async (req, res) => {
     } 
 });
 
+
+profileRouter.patch('/profile/password', userAuth, async (req, res) => {
+    try{
+        const user = req.user;
+        const { oldpassword, newpassword } = req.body;
+        const isvalid = await user.validatePassword(oldpassword);
+        if(!isvalid){
+            throw new Error("Invalid old password");
+        }
+        user.password = newpassword;
+        await user.save();
+        res.send(`${user.firstname} your password updated successfully`);
+    }catch(err){
+        res.send("error occured"+ err.message);
+    }
+});
 
 module.exports = profileRouter;  
