a077a9b03e2405904f8857aecea0a12c7793d230
Rohit Nagote <rohitnagote01@gmail.com>
2026-09-08 23:50:08 +0530

    request api

--- src/routes/request.js
+++ src/routes/request.js
@@ -2,9 +2,14 @@ const express = require('express');
 
 const requestRouter = express.Router();
 const { userAuth } = require('../middleware/auth.js');
+const ConnectionRequest = require('../models/connectionRequest.js');
 
-requestRouter.post('/request', userAuth, async (req, res) => {
+requestRouter.post('/request/send/intrested/:receiverId', userAuth, async (req, res) => {
     try{
+        const senderId = req.user._id;
+        const receiverId = req.params.receiverId;
+        const status = req.params.receiverId;
+
 
     }catch(err){
         res.send("error occured"+ err.message);
