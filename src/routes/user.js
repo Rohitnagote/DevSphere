2f316db4451231631c12225d412af932df1ca9fe
Rohit Nagote <rohitnagote01@gmail.com>
2026-09-16 20:07:47 +0530

    user/connections api

--- src/routes/user.js
+++ src/routes/user.js
@@ -21,5 +21,34 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     res.status(500).json({ message: "Error occurred: " + err.message });
   }
 });
+
+
+//get all the connections of the logged in user
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try{
+    const loggedInUser = req.user;
+
+    const connectionRequests = await connectionRequestModel.find({
+      $or:[{ senderId: loggedInUser._id, status: "accepted"},
+        { receiverId: loggedInUser._id, status: "accepted" }
+      ]
+  }).populate("senderId", "firstname lastname skills").populate("receiverId", "firstname lastname skills");
+  
+  const data = connectionRequests.map((row) => {
+    if (row.senderId._id.toString() === loggedInUser._id.toString()) {
+      return row.receiverId;
+    }
+    return row.senderId;
+  });
+
+  res.json({data});
+
+  }catch(err){
+    res.status(500).json({ message: "Error occurred: " + err.message });
+  }
+
+});
+
+
       
 module.exports = userRouter;
